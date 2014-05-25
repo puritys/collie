c6de7b508d2a127850335071c3023556e1c61761
puritys <pipi00000@gmail.com>
2014-05-25 22:19:48 +0800

    add template of case and header, footer

---
+++ web/templates/base.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html class="auction ua-desktop ua-chrome ua-chrome30 ua-Mac" lang="zh-tw">
+<head>    
+    <title>Easy Tester</title>
+    <meta charset="utf-8" />
+    <link href="/<?php echo PROJECT;?>/css/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <?php include $tempFile['layout'];?>
+
+</body>
+</html>
