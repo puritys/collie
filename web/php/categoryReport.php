10c2cebcfba6728dc6372981873f39d6b09de17c
puritys <pipi00000@gmail.com>
2014-06-22 22:47:52 +0800

    add chart

--- web/php/categoryReport.php
+++ web/php/categoryReport.php
@@ -43,6 +43,38 @@ for ($i = 0; $i < $n; $i++) {
 
 }
 
+
+if (!empty($reportGroup['passed_case_num']) || !empty($reportGroup['failed_case_num'])) {
+    require_once PATH_WEB . '/lib/jpgraph/jpgraph.php';
+    require_once PATH_WEB . '/lib/jpgraph/jpgraph_pie.php';
+
+
+    $dataChart = array($reportGroup['passed_case_num'], $reportGroup['failed_case_num']);
+
+    // Create the Pie Graph. 
+    $graph = new PieGraph(350,250);
+
+    $theme_class="DefaultTheme";
+    //$graph->SetTheme(new $theme_class());
+
+    // Set A title for the plot
+    $graph->title->Set("Test Result");
+    $graph->SetBox(true);
+
+    // Create
+    $p1 = new PiePlot($dataChart);
+    $graph->Add($p1);
+
+    $p1->ShowBorder();
+    $p1->SetColor('black');
+    $p1->SetSliceColors(array('#1Ecc11','#cc1111'));
+    if (!is_dir(PATH_DIR_RUN . "/reportChart")) {
+        mkdir(PATH_DIR_RUN . "/reportChart");
+    }
+    $img = $graph->Stroke(PATH_DIR_RUN . "/reportChart/char_$reportGroupId.png");
+    $data['chart'] = URL_DIR_RUN . "/reportChart/char_$reportGroupId.png";
+}
+
 $body = array(
     array("grid" => "u-1", "T" => "report/list/list.hb.html", "dataObject" => $data),
 );
